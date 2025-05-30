2d52b21b7d70fcddc64965dd1e018579b3444569
Alejandro <20240657@ricaldone.edu.sv>
2025-05-30 10:44:44 -0600

    #
    git commit -m #

---
+++ Ejercicio1_PruebaRepetitiva/src/main/java/DiegoLopez_20240657/Main.java
@@ -0,0 +1,24 @@
+package DiegoLopez_20240657;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        double numero = scanner.nextDouble();
+        
+        if (numero > 0) {
+            System.out.println("El numero es positivo");
+        } else if (numero < 0) {
+            System.out.println("El numero es negativo");
+        }
+        else {
+            System.out.println("El numero es 0");
+        }
+
+    }
+}
